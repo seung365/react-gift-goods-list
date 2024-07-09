b4079a51a6ec8db8e66f44feef4744509ecdaad7
seung365 <bdh3659@naver.com>
2024-07-10 02:28:28 +0900

    feat: GoodsRankingSection 구현

--- src/components/features/Home/GoodsRankingSection/index.tsx
+++ src/components/features/Home/GoodsRankingSection/index.tsx
@@ -1,10 +1,11 @@
 import styled from '@emotion/styled';
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 import { Container } from '@/components/common/layouts/Container';
 import { breakpoints } from '@/styles/variants';
 import type { RankingFilterOption } from '@/types';
-import { GoodsMockList } from '@/types/mock';
+import type { GoodsData } from '@/types';
 
 import { GoodsRankingFilter } from './Filter';
 import { GoodsRankingList } from './List';
@@ -14,15 +15,26 @@ export const GoodsRankingSection = () => {
     targetType: 'ALL',
     rankType: 'MANY_WISH',
   });
-
-  // GoodsMockData를 21번 반복 생성
+  const [GoodsItem, setGoodsItem] = useState<GoodsData[]>([]);
+  useEffect(() => {
+    const fetchGoods = async () => {
+      const url = `https://react-gift-mock-api-seungbeom.vercel.app/api/v1/ranking/products?targetType=${filterOption.targetType}&rankType=${filterOption.rankType}`;
+      try {
+        const response = await axios.get(url);
+        setGoodsItem(response.data.products);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchGoods();
+  }, [filterOption]);
 
   return (
     <Wrapper>
       <Container>
         <Title>실시간 급상승 선물랭킹</Title>
         <GoodsRankingFilter filterOption={filterOption} onFilterOptionChange={setFilterOption} />
-        <GoodsRankingList goodsList={GoodsMockList} />
+        <GoodsRankingList goodsList={GoodsItem} />
       </Container>
     </Wrapper>
   );
