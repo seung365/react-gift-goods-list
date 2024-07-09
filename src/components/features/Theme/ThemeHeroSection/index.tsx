661aec435285c45e8b8fe2b1fb31a558cf4049b4
seung365 <bdh3659@naver.com>
2024-07-10 02:27:18 +0900

    feat: Theme Header 구현

--- src/components/features/Theme/ThemeHeroSection/index.tsx
+++ src/components/features/Theme/ThemeHeroSection/index.tsx
@@ -1,16 +1,17 @@
 import styled from '@emotion/styled';
 
 import { Container } from '@/components/common/layouts/Container';
+import { useThemeContext } from '@/provider/API/ThemeContext';
 import { breakpoints } from '@/styles/variants';
 import type { ThemeData } from '@/types';
-import { ThemeMockList } from '@/types/mock';
 
 type Props = {
   themeKey: string;
 };
 
 export const ThemeHeroSection = ({ themeKey }: Props) => {
-  const currentTheme = getCurrentTheme(themeKey, ThemeMockList);
+  const { themes } = useThemeContext();
+  const currentTheme = getCurrentTheme(themeKey, themes);
 
   if (!currentTheme) {
     return null;
