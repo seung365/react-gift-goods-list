49864c4b0e6d967ebd57de24c46c53513b8d7613
seung365 <bdh3659@naver.com>
2024-07-11 01:25:05 +0900

    feat: GoodsRankingSection loading과 error 처리

--- src/components/features/Home/GoodsRankingSection/index.tsx
+++ src/components/features/Home/GoodsRankingSection/index.tsx
@@ -16,14 +16,20 @@ export const GoodsRankingSection = () => {
     rankType: 'MANY_WISH',
   });
   const [GoodsItem, setGoodsItem] = useState<GoodsData[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
   useEffect(() => {
     const fetchGoods = async () => {
       const url = `https://react-gift-mock-api-seungbeom.vercel.app/api/v1/ranking/products?targetType=${filterOption.targetType}&rankType=${filterOption.rankType}`;
       try {
         const response = await axios.get(url);
         setGoodsItem(response.data.products);
+        setErrorMessage(null);
       } catch (error) {
         console.error(error);
+        setErrorMessage('데이터를 불러오는 중에 문제가 발생했습니다.');
+      } finally {
+        setLoading(false);
       }
     };
     fetchGoods();
@@ -34,7 +40,7 @@ export const GoodsRankingSection = () => {
       <Container>
         <Title>실시간 급상승 선물랭킹</Title>
         <GoodsRankingFilter filterOption={filterOption} onFilterOptionChange={setFilterOption} />
-        <GoodsRankingList goodsList={GoodsItem} />
+        <GoodsRankingList goodsList={GoodsItem} loading={loading} errorMessage={errorMessage} />
       </Container>
     </Wrapper>
   );
