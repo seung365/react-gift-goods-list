af62d0f7944b0ea2e22d33133c751eb3bdb16b49
seung365 <bdh3659@naver.com>
2024-07-11 01:27:13 +0900

    feat: ThemeGoodsSection 에러 처리

--- src/components/features/Theme/ThemeGoodsSection/index.tsx
+++ src/components/features/Theme/ThemeGoodsSection/index.tsx
@@ -12,24 +12,38 @@ type Props = {
 };
 
 export const ThemeGoodsSection = ({ themeKey }: Props) => {
-  const [Goods, setGoods] = useState<GoodsData[]>([]);
+  const [goods, setGoods] = useState<GoodsData[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [rending, setRending] = useState(true);
+  const maxRetries = 5;
+  const retryDelay = 1000;
 
   useEffect(() => {
-    const fetchGoods = async () => {
+    const fetchGoods = async (retries = 0) => {
       const url = `https://react-gift-mock-api-seungbeom.vercel.app/api/v1/themes/${themeKey}/products?maxResults=20`;
       try {
         const response = await axios.get(url);
         setGoods(response.data.products);
+        setLoading(false);
       } catch (error) {
         console.error(error);
+        if (retries < maxRetries) {
+          setTimeout(() => fetchGoods(retries + 1), retryDelay);
+        } else {
+          setLoading(false);
+          setRending(false);
+        }
       }
     };
+
     fetchGoods();
-  });
+  }, [themeKey]);
 
   return (
     <Wrapper>
-      <Container>
+      <Container alignItems="center">
+        {loading && <Spinner />}
+        {!rending && <div>에러가 발생했습니다.</div>}
         <Grid
           columns={{
             initial: 2,
@@ -37,7 +51,8 @@ export const ThemeGoodsSection = ({ themeKey }: Props) => {
           }}
           gap={16}
         >
-          {Goods.map(({ id, imageURL, name, price, brandInfo }) => (
+          {' '}
+          {goods.map(({ id, imageURL, name, price, brandInfo }) => (
             <DefaultGoodsItems
               key={id}
               imageSrc={imageURL}
@@ -58,5 +73,26 @@ const Wrapper = styled.section`
 
   @media screen and (min-width: ${breakpoints.sm}) {
     padding: 40px 16px 360px;
+  }
+`;
+
+const Spinner = styled.div`
+  border: 4px solid rgba(0, 0, 0, 0.1);
+  border-left-color: #22a6b3;
+  border-radius: 50%;
+  width: 36px;
+  height: 36px;
+  animation: spin 1s linear infinite;
+  align-items: center;
+  justify-content: center;
+  display: flex;
+
+  @keyframes spin {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
 `;
