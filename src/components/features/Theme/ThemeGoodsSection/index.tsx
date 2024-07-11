f741dc43c047b2f9a2633bd288fd1b4765ae59e0
seung365 <bdh3659@naver.com>
2024-07-11 15:41:50 +0900

    refactor: ThemeGoodsSection index 코드 수정

--- src/components/features/Theme/ThemeGoodsSection/index.tsx
+++ src/components/features/Theme/ThemeGoodsSection/index.tsx
@@ -6,10 +6,12 @@ import { useEffect, useState } from 'react';
 import { DefaultGoodsItems } from '@/components/common/GoodsItem/Default';
 import { Container } from '@/components/common/layouts/Container';
 import { Grid } from '@/components/common/layouts/Grid';
+import { Spinner } from '@/components/common/Spinner';
 import { breakpoints } from '@/styles/variants';
 import type { GoodsData } from '@/types';
 import type { GoodsResponse } from '@/types';
 import { handleError } from '@/utils/errorHandler';
+
 type Props = {
   themeKey: string;
 };
@@ -76,26 +78,5 @@ const Wrapper = styled.section`
 
   @media screen and (min-width: ${breakpoints.sm}) {
     padding: 40px 16px 360px;
-  }
-`;
-
-const Spinner = styled.div`
-  border: 4px solid rgba(0, 0, 0, 0.1);
-  border-left-color: #22a6b3;
-  border-radius: 50%;
-  width: 36px;
-  height: 36px;
-  animation: spin 1s linear infinite;
-  align-items: center;
-  justify-content: center;
-  display: flex;
-
-  @keyframes spin {
-    0% {
-      transform: rotate(0deg);
-    }
-    100% {
-      transform: rotate(360deg);
-    }
   }
 `;
