59a6ac0de7c4d6afe2493f231451c3f80aac242b
seung365 <bdh3659@naver.com>
2024-07-12 21:04:08 +0900

    style: Auth provider 코드 주석 삭제

--- src/provider/Auth/index.tsx
+++ src/provider/Auth/index.tsx
@@ -14,13 +14,12 @@ export const AuthContext = createContext<AuthInfo | undefined>(undefined);
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const currentAuthToken = authSessionStorage.get();
   const [isReady, setIsReady] = useState(!currentAuthToken);
-
   const [authInfo, setAuthInfo] = useState<AuthInfo | undefined>(undefined);
 
   useEffect(() => {
     if (currentAuthToken) {
       setAuthInfo({
-        id: currentAuthToken, // TODO: 임시로 로그인 페이지에서 입력한 이름을 ID, token, name으로 사용
+        id: currentAuthToken,
         name: currentAuthToken,
         token: currentAuthToken,
       });
