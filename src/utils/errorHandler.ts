5068cccd188b49fa1dc3089c8f2eee7eda0511c4
seung365 <bdh3659@naver.com>
2024-07-15 10:28:42 +0900

    feat: errorHandler에서 error 메시지 상수 처리

--- src/utils/errorHandler.ts
+++ src/utils/errorHandler.ts
@@ -1,18 +1,20 @@
 import type { AxiosError } from 'axios';
 import axios from 'axios';
 
+import { HTTP_ERROR_MESSAGES } from './errorMessages';
+
 export const handleError = (error: AxiosError): string => {
   if (axios.isAxiosError(error) && error.response) {
     const status = error.response.status;
     if (status >= 100 && status < 200) {
-      return '정보 제공 응답입니다. 계속 진행해 주세요.';
+      return HTTP_ERROR_MESSAGES.INFORMATIONAL_RESPONSE;
     } else if (status >= 300 && status < 400) {
-      return '리다이렉션이 필요합니다. 요청한 URI가 이동되었습니다.';
+      return HTTP_ERROR_MESSAGES.REDIRECTION_REQUIRED;
     } else if (status >= 400 && status < 500) {
-      return '데이터를 불러오는 중에 문제가 발생했습니다.';
+      return HTTP_ERROR_MESSAGES.CLIENT_ERROR;
     } else if (status >= 500) {
-      return '서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.';
+      return HTTP_ERROR_MESSAGES.SERVER_ERROR;
     }
   }
-  return '알 수 없는 오류가 발생했습니다.';
+  return HTTP_ERROR_MESSAGES.UNKNOWN_ERROR;
 };
