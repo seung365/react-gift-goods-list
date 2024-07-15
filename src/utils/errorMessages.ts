9531c0e845fd7d12d9330c2ef992543ad90c9112
seung365 <bdh3659@naver.com>
2024-07-15 10:28:20 +0900

    feat: errorMessages 추가

---
+++ src/utils/errorMessages.ts
@@ -0,0 +1,7 @@
+export const HTTP_ERROR_MESSAGES = {
+  INFORMATIONAL_RESPONSE: '정보 제공 응답입니다. 계속 진행해 주세요.',
+  REDIRECTION_REQUIRED: '리다이렉션이 필요합니다. 요청한 URI가 이동되었습니다.',
+  CLIENT_ERROR: '데이터를 불러오는 중에 문제가 발생했습니다.',
+  SERVER_ERROR: '서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.',
+  UNKNOWN_ERROR: '알 수 없는 오류가 발생했습니다.',
+};
